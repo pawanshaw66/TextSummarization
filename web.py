82e2447e010e08e6b454c7091a7b2f15f0477616
pawanshaw66 <97239563+pawanshaw66@users.noreply.github.com>
2023-04-25 19:31:16 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,24 @@
+
+
+from flask import Flask, render_template, request
+from text_summarization import summarizer
+
+web = Flask(__name__)
+@web.route('/')
+
+def index():
+    return render_template('index.html')
+
+@web.route('/analyze',methods=['GET','POST'])
+def analyze():
+    if request.method=='POST':
+        rawtext=request.form['rawtext']
+        summary,original_txt, len_orig_txt, len_summary = summarizer(rawtext)
+        return render_template('summary.html',summary=summary, original_txt=original_txt,len_orig_txt=len_orig_txt,len_summary=len_summary)
+    
+
+if __name__=='__main__':
+    web.run(debug=True)
+    
+    
+ 
